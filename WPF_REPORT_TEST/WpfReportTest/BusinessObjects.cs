7ac4ce29ace7ed6b9c507ab44da3f66d320dc2f6
hhjtfb <slfb365@163.com>
2018-08-06 15:19:17 +0800

    add porject

---
+++ WPF_REPORT_TEST/WpfReportTest/BusinessObjects.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfReportTest
+{
+    class BusinessObjects
+    {
+        public class Product
+        {
+            private string m_name;
+            private int m_price;
+
+            public Product(string name, int price)
+            {
+                m_name = name;
+                m_price = price;
+            }
+
+            public string Name
+            {
+                get
+                {
+                    return m_name;
+                }
+            }
+
+            public int Price
+            {
+                get
+                {
+                    return m_price;
+                }
+            }
+        }
+
+        public class Merchant
+        {
+            private List<Product> m_products;
+
+            public Merchant()
+            {
+
+                m_products = new List<Product>();
+
+                m_products.Add(new Product("Pen", 25));
+
+                m_products.Add(new Product("Pencil", 30));
+
+                m_products.Add(new Product("Notebook", 15));
+
+            }
+
+            public List<Product> GetProducts()
+            {
+                return m_products;
+            }
+        }
+    }
+}
---
+++ WPF_REPORT_TEST/WpfReportTest/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfReportTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WpfReportTest.BusinessObjects.Merchant m_merchant =
+            new WpfReportTest.BusinessObjects.Merchant();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRpt_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new
+                Microsoft.Reporting.WinForms.ReportDataSource();
+
+            reportDataSource1.Name = "DataSet1"; //Name of the report dataset in our .RDLC file  
+            reportDataSource1.Value = m_merchant.GetProducts();//重要部分  
+            this._reportViewer.Reset();
+            this._reportViewer.LocalReport.DataSources.Add(reportDataSource1);
+            this._reportViewer.LocalReport.ReportEmbeddedResource = "WpfReportTest.Report1.rdlc";
+            _reportViewer.RefreshReport();
+        }
+    }
+}
